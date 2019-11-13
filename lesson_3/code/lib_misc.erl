d2c77f2b6ab14ece5a957f17c1151b90ddd0f3f0
yuya okabe <takamizawa46@gmail.com>
2019-11-14 07:59:09 +0900

    finished lesson3

--- lesson_3/code/lib_misc.erl
+++ lesson_3/code/lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([for/3, qsort/1, pythag/1]).
+-export([for/3, qsort/1, pythag/1, perms/1, filter/2, odds_and_evens_acc/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F) -> [F(I)|for(I+1, Max, F)].
@@ -21,3 +21,22 @@ pythag(N) -> [{A, B, C} ||
 % [[1,2,3,4 | 2
 perms([]) -> [ [] ];
 perms(L) -> [ [H|T] || H <- L, T <- perms(L--[H])].
+
+% original filter funtion
+% user case syntax
+filter(_, []) -> [];
+filter(P, [H|T]) -> 
+    case P(H) of
+        true -> [H|filter(P, T)];
+        false -> [filter(P, T)]
+    end.
+
+% example for accumlator. create Odd, Even list
+odds_and_evens_acc(L) -> odds_and_evens_acc(L, [], []).
+
+odds_and_evens_acc([H|T], Odds, Evens) -> 
+    case (H rem 2) of
+        1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+        0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+    end;
+odds_and_evens_acc([], Odds, Evens) -> {lists:reverse(Odds), lists:reverse(Evens)}.
--- lesson_3/note.md
+++ lesson_3/note.md
@@ -26,6 +26,7 @@ Eshell V10.2.3  (abort with ^G)
 -module(file). % モジュールに対する命名。基本的にファイル名と同じで良いのかな?
 -export([func/1]). % 外部から使用したい関数を列挙(リストなのでおそらく複数記述出来る) /Nは引数の数
 - import(module_name, [func/1, func/2]
+- include(file_path) % ```.hrl```ファイル等(defined in record)をimportする際に使用
 
 somefunc([]) -> 0; % 同名の関数(アリティの数も同じ)をまだ記述する場合には```;```を使用
 somefunc([H|T]) -> somefunc(T). % 終了を宣言する```.```を使用
@@ -36,7 +37,7 @@ syntaxについて
 ```erlang
 % L = [1,2,3,4,5]
 [X * 2 || X <- L]
-````
+````i
 
 generatorを使ってfilterを行う場合
 ```erlang
@@ -58,5 +59,29 @@ goでのstructのようなもの
 X = #{todo}.
 
 % update record
-X1 = X#todo{status=offline}
+X1 = X#todo{status=offline}.
+
+% pattern match
+#todo{status=S, who=W} = X1.
+
+% dot operator
+X1#todo.status.
+```
+
+## 条件式
+**case**
+```erlang
+case P(H) of
+    true -> __;
+    false -> __
+end;
+```
+
+**if**
+```erlang
+if 
+    pattern1 -> __;
+    pattern2 -> __;
+    __ -> __
+end;
 ```
